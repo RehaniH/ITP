9d80b496ac050d77bc61bb8d0a2f26b04f99da13
tharikrizan <tharik.r@yahoo.com>
2019-09-29 23:22:33 +0530

    stock completee

---
+++ ITP/src/com/nlhs/servlet/DeleteItemServlet.java
@@ -0,0 +1,42 @@
+package com.nlhs.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.nlhs.service.ItemsServiceImp;
+
+/**
+ * Servlet implementation class DeleteItemServlet
+ */
+@WebServlet("/DeleteItemServlet")
+public class DeleteItemServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public DeleteItemServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String id = request.getParameter("product_id");
+		System.out.println("delete Servlet:" + id);
+		ItemsServiceImp isi = new ItemsServiceImp();
+		String message = isi.deleteItem(id);
+		request.setAttribute("Deletemessage", message);
+		getServletContext().getRequestDispatcher("/ShowProductForm.jsp").forward(request, response);
+	}
+
+}
